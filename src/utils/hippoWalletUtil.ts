ebdd3a3026c8920ddadfa30a8b34030dff32b536
mana <mana@hippo.space>
2022-09-03 14:02:57 +0800

    use latest sdk 0.3.7

--- src/utils/hippoWalletUtil.ts
+++ src/utils/hippoWalletUtil.ts
@@ -1,36 +1,7 @@
-import { getTypeTagFullname, StructTag, TypeTag } from '@manahippo/move-to-ts';
 import { CONFIGS, NetworkConfiguration } from '@manahippo/hippo-sdk';
-import { AptosAccount, AptosClient, Types } from 'aptos';
 
 export const readConfig = (): NetworkConfiguration => {
   const isDevnet = true;
   const netConf = isDevnet ? CONFIGS.devnet : CONFIGS.localhost;
   return netConf;
-};
-
-export async function sendPayloadTx(
-  client: AptosClient,
-  account: AptosAccount,
-  payload: Types.TransactionPayload,
-  max_gas = 1000
-) {
-  const txnRequest = await client.generateTransaction(account.address(), payload, {
-    max_gas_amount: `${max_gas}`
-  });
-  const signedTxn = await client.signTransaction(account, txnRequest);
-  const txnResult = await client.submitTransaction(signedTxn);
-  await client.waitForTransaction(txnResult.hash);
-  const txDetails = (await client.getTransactionByHash(txnResult.hash)) as Types.UserTransaction;
-  console.log(txDetails);
-}
-
-export const getJointName = (xTag: TypeTag, yTag: TypeTag) => {
-  if (!(xTag instanceof StructTag)) {
-    throw new Error(`Expected xTag to be StructTag but received: ${JSON.stringify(xTag)}`);
-  }
-  if (!(yTag instanceof StructTag)) {
-    throw new Error(`Expected yTag to be StructTag but received: ${JSON.stringify(yTag)}`);
-  }
-  const [xFullname, yFullname] = [xTag, yTag].map(getTypeTagFullname);
-  return `${xFullname}/${yFullname}`;
 };
