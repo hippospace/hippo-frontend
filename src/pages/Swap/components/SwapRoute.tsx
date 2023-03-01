853ee2f0126c2ea4c637713cc22493bfc66de1df
robbinh <robin@hippo.space>
2023-03-01 23:35:51 +0800

    Update routes icons

--- src/pages/Swap/components/SwapRoute.tsx
+++ src/pages/Swap/components/SwapRoute.tsx
@@ -4,16 +4,32 @@ import { GeneralRouteAndQuote } from 'types/hippo';
 import NonSplitSwapDexes, { SplitSingleRouteDexes } from './SwapDexes';
 import TokenSteps from './TokenSteps';
 
-const APITradeRoute = ({ r }: { r: AggregatorTypes.ApiTradeRoute }) => {
+const APITradeRoute = ({
+  r,
+  isShowDetails = false
+}: {
+  r: AggregatorTypes.ApiTradeRoute;
+  isShowDetails?: boolean;
+}) => {
   return (
     <div className="flex items-center justify-between">
       <TokenSteps tokens={r.tokens} />
-      <NonSplitSwapDexes r={r} />
+      <NonSplitSwapDexes r={r} isShowDetails={isShowDetails} />
     </div>
   );
 };
 
-const SplitSingleRouteComp = ({ r, scale }: { r: SplitSingleRoute; scale?: number }) => {
+const SplitSingleRouteComp = ({
+  r,
+  scale,
+  isShowDetails,
+  isInMultiRoute = true
+}: {
+  r: SplitSingleRoute;
+  scale?: number;
+  isShowDetails?: boolean;
+  isInMultiRoute?: boolean;
+}) => {
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center min-w-0">
@@ -24,30 +40,46 @@ const SplitSingleRouteComp = ({ r, scale }: { r: SplitSingleRoute; scale?: numbe
         )}
         <TokenSteps tokens={r.tokens} className="truncate" />
       </div>
-      <SplitSingleRouteDexes sr={r} />
+      <SplitSingleRouteDexes sr={r} isShowDetails={isShowDetails} isInMultiRoute={isInMultiRoute} />
     </div>
   );
 };
 
-const SplitMultiRouteComp = ({ r }: { r: SplitMultiRoute }) => {
+const SplitMultiRouteComp = ({
+  r,
+  isShowDetails
+}: {
+  r: SplitMultiRoute;
+  isShowDetails?: boolean;
+}) => {
   return (
     <div className="space-y-1">
       {[...r.units]
         .sort((a, b) => b.scale - a.scale)
         .map((u, index) => {
-          return <SplitSingleRouteComp key={index} r={u.route} scale={u.scale} />;
+          return (
+            <SplitSingleRouteComp
+              key={index}
+              r={u.route}
+              scale={u.scale}
+              isShowDetails={isShowDetails}
+              isInMultiRoute={true}
+            />
+          );
         })}
     </div>
   );
 };
 
-const SwapRoute = ({ r }: { r: GeneralRouteAndQuote }) => {
+const SwapRoute = ({ r, isShowDetails }: { r: GeneralRouteAndQuote; isShowDetails?: boolean }) => {
   if (r.route instanceof AggregatorTypes.ApiTradeRoute) {
-    return <APITradeRoute r={r.route} />;
+    return <APITradeRoute r={r.route} isShowDetails={isShowDetails} />;
   } else if (r.route instanceof AggregatorTypes.SplitSingleRoute) {
-    return <SplitSingleRouteComp r={r.route} />;
+    return (
+      <SplitSingleRouteComp r={r.route} isShowDetails={isShowDetails} isInMultiRoute={false} />
+    );
   } else if (r.route instanceof AggregatorTypes.SplitMultiRoute) {
-    return <SplitMultiRouteComp r={r.route} />;
+    return <SplitMultiRouteComp r={r.route} isShowDetails={isShowDetails} />;
   } else {
     throw new Error('Invalid route type for SwapRoute');
   }
